9f7bbbf15135240ba95e9ec50004a084aefd42c1
charlotte <charlotteblond81@gmail.com>
2023-08-16 22:25:18 +0200

    Added Imgui and Imgui-SFML wip make file

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,68 @@
-#include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+// #include <SFML/Graphics.hpp>
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
+// int main()
+// {
+//     auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+//     window.setFramerateLimit(144);
+
+//     while (window.isOpen())
+//     {
+//         for (auto event = sf::Event{}; window.pollEvent(event);)
+//         {
+//             if (event.type == sf::Event::Closed)
+//             {
+//                 window.close();
+//             }
+//         }
+
+//         window.clear();
+//         window.display();
+//     }
+// }
+
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    bool test = ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(window, event);
+
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
 
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
         window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
         window.display();
     }
+
+    ImGui::SFML::Shutdown();
+
+    return 0;
 }
